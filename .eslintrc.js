d86bb7036229776b3fce2ad031a809281f6b2dbc
Leonid Cupikov <dreambooms@gmail.com>
2021-08-13 22:16:28 +0300

    Initialize project using Create React App

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 2020,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	rules: {
+		'eqeqeq': ['error', 'always'],
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+	},
+};
